a2b7585533ae8293266ba325413c8c1f17b25e51
Maja Sørensen <majabirchsorensen@gmail.com>
2022-05-20 14:00:01 +0200

    dummy prouktside

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ new Swiper('.swiper', {
   })
 
 //   Trustpilot sektion swiper
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     new Swiper('.swiper-container', {
       loop: true,
       slidesPerView: 1,
@@ -43,10 +43,83 @@ new Swiper('.swiper', {
     })
   })
 
-  // Cookie close btn
-  window.onload = () => {
+//   Produktanmeldelse sektion swiper
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.swiper-containerAnmeldelse', {
+    loop: true,
+      slidesPerView: 1,
+      spaceBetween: 32,
+      autoplay: {
+        delay: 8000,
+      },
+      breakpoints: {
+        640: {
+          centeredSlides: true,
+          slidesPerView: 1.5,
+        },
+        1024: {
+          centeredSlides: false,
+          slidesPerView: 3,
+        },
+      },
+      navigation: {
+        nextEl: '.next-button',
+        prevEl: '.prev-button',
+      },
+    })
+})
+
+// Cookie close btn
+window.onload = () => {
     document.getElementById('close').onclick = function() {
       this.parentNode.remove()
       return false;
     };
   };
+
+ 
+
+
+// Produktside valgt knap farve
+  
+  // grab all the buttons
+  let Buttons = document.querySelectorAll(".selectSection button");
+
+  // loop through the buttons using for..of 
+  for (let button of Buttons) {
+
+  // listen for a click event 
+  button.addEventListener('click', (e) => {
+  // Store the event target in a const
+  const et = e.target;
+
+  // select active class
+  const active = document.querySelector(".active");
+
+  /* when a button is clicked, remove the active class 
+  from the button that has it */
+  if (active) {
+    active.classList.remove("active");
+   }
+
+  // Add active class to the clicked element
+  et.classList.add("active");
+
+  // select all classes with the name content
+  let allContent = document.querySelectorAll('.content');
+
+  // loop through all content classes
+  for (let content of allContent) {
+
+    /* display the content if the value in the data attribute of the button and content are the same */
+    if(content.getAttribute('data-number') === button.getAttribute('data-number')) {
+      content.style.display = "block";
+     }
+
+     // if it's not equal then hide it.
+     else {
+        content.style.display = "none";
+      }
+    }
+ });
+}
